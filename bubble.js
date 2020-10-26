737810cd687f9ae367cfd9bd6a101233ca5acc82
Ilya Sviridov <30390287+SynteticCat@users.noreply.github.com>
2020-10-26 12:08:42 +0300

    Update bubble.js

--- bubble.js
+++ bubble.js
@@ -15,9 +15,7 @@ const bubbleSort = (arr, comparation) =>
    const n = arr.length;
    for (let i = 0; i < n-1; i++) { 
        for (let j = 0; j < n-1-i; j++) { 
-           // сравниваем элементы
            if (comparation(arr[j], arr[j+1])) { 
-               // делаем обмен элементов
                let temp = arr[j+1]; 
                arr[j+1] = arr[j]; 
                arr[j] = temp; 
